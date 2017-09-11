cc5f53c95625b63d70ce438c6949b4b394ca6de9
汪淼 <1071597946@qq.com>
2017-09-11 17:28:38 +0800

    Initial Commit

---
+++ 设置/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  设置
+//
+//  Created by baiju on 2017/9/11.
+//  Copyright © 2017年 baiju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 设置/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  设置
+//
+//  Created by baiju on 2017/9/11.
+//  Copyright © 2017年 baiju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
